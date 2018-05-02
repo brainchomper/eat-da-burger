e1fb17808f13455c983ffd5ae9007d2b32a23441
Kevin Smith <kevin@kvnsmith.com>
2018-05-02 03:44:05 -0600

    working files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapino Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fungus Among Us Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Pepper Jack Burger', true);
